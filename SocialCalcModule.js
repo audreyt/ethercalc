c8abd1a92ac7aae584a36c5824bd4d68a944b714
Audrey Tang <audreyt@audreyt.org>
2012-09-29 17:38:23 +0800

    * "page"!A1 should now work in pure-js environments too.

--- SocialCalcModule.js
+++ SocialCalcModule.js
@@ -25231,6 +25231,8 @@ if (typeof global != 'undefined') var window = global;
 if (typeof SocialCalc != 'undefined' && typeof module != 'undefined') module.exports = SocialCalc;
 
 // We don't really need a DOM-based presentation layer for embedded SC.
+SocialCalc.GetEditorCellElement = function () {};
+SocialCalc.ReplaceCell = function () {};
 SocialCalc.EditorRenderSheet = function () {};
 SocialCalc.SpreadsheetControlSortSave = function () {};
 SocialCalc.SpreadsheetControlStatuslineCallback = function () {};
--- exports.js
+++ exports.js
@@ -2,11 +2,13 @@ if (typeof global != 'undefined') var window = global;
 if (typeof SocialCalc != 'undefined' && typeof module != 'undefined') module.exports = SocialCalc;
 
 // We don't really need a DOM-based presentation layer for embedded SC.
+SocialCalc.GetEditorCellElement = function () {};
+SocialCalc.ReplaceCell = function () {};
 SocialCalc.EditorRenderSheet = function () {};
 SocialCalc.SpreadsheetControlSortSave = function () {};
 SocialCalc.SpreadsheetControlStatuslineCallback = function () {};
-SocialCalc.DoPositionCalculations = function () {
+SocialCalc.DoPositionCalculations = function (editor) {
     SocialCalc.EditorSheetStatusCallback(
-        null, "doneposcalc", null, SocialCalc.EditorStepInfo.editor
+        null, "doneposcalc", null, editor
     );
 }
