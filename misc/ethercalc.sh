61b873766aa45f0068b43b9885275f793076302c
唐鳳 <audreyt@audreyt.org>
2014-11-21 13:35:24 +0800

    Update ethercalc.sh

--- misc/ethercalc.sh
+++ misc/ethercalc.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker.io run -t -i -c 4 -d -p 127.0.0.1:6379:6379 -p 6967:6967 -v /var/lib/docker/binds/ethercalc-redis:/redis:rw audreyt/ethercalc
+docker run --name redis -d -v /var/lib/docker/binds/ethercalc-redis:/data redis:latest
+docker run -d -p 6967:8000 --link redis:redis audreyt/ethercalc
