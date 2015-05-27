0ad9d0fbadbaec50ef4d185f02836cbba07922a5
Patrick Robertson <robertson.patrick@gmail.com>
2015-05-27 10:49:23 +0800

    Allow shift+enter to search backwards

--- SocialCalcModule.js
+++ SocialCalcModule.js
@@ -22243,7 +22243,11 @@ spreadsheet.Buttons = {
    input.keyup(function (e) {
         if (e.keyCode == 13) {
            // search down when enter is pressed
-           SocialCalc.SpreadsheetControl.SearchDown();
+           if (e.shiftKey) {
+               SocialCalc.SpreadsheetControl.SearchUp();
+           } else {
+               SocialCalc.SpreadsheetControl.SearchDown();
+           }
         }
    });
    spreadsheet.formulabarDiv.appendChild(searchBar[0]);
